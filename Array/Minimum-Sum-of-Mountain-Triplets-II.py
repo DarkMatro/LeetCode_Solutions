01403d221954325fe0a733dfa15e0bc03da44ee6
Oleg Frolov <frolovaleh@gmail.com>
2024-03-12 18:36:35 +0400

    last solved task ID 2909 (total 123)

---
+++ Array/Minimum-Sum-of-Mountain-Triplets-II.py
@@ -0,0 +1,56 @@
+from itertools import accumulate
+
+
+def minimumSum(nums: list[int]) -> int:
+    """
+    ID: 2909
+    Tags:   Array
+    Time:   O(N)
+    Memory: O(N)
+
+    Task
+    ----------
+    You are given a 0-indexed array nums of integers.
+
+    A triplet of indices (i, j, k) is a mountain if:
+
+    i < j < k
+    nums[i] < nums[j] and nums[k] < nums[j]
+    Return the minimum possible sum of a mountain triplet of nums. If no such triplet exists, return -1.
+
+    Parameters
+    ----------
+    nums : list[int]
+
+    Returns
+    -------
+    out : int
+
+    Examples
+    --------
+    >>> minimumSum([8,6,1,5,3])
+    9
+
+    Explanation: Triplet (2, 3, 4) is a mountain triplet of sum 9 since:
+    - 2 < 3 < 4
+    - nums[2] < nums[3] and nums[4] < nums[3]
+    And the sum of this triplet is nums[2] + nums[3] + nums[4] = 9. It can be shown that there are no mountain triplets
+    with a sum of less than 9.
+
+    >>> minimumSum([5,4,8,7,10,2])
+    13
+
+    Explanation: Triplet (1, 3, 5) is a mountain triplet of sum 13 since:
+    - 1 < 3 < 5
+    - nums[1] < nums[3] and nums[5] < nums[3]
+    And the sum of this triplet is nums[1] + nums[3] + nums[5] = 13. It can be shown that there are no mountain triplets
+    with a sum of less than 13.
+
+    >>> minimumSum([6,5,4,3,4,5])
+    -1
+
+    Explanation: It can be shown that there are no mountain triplets in nums.
+    """
+    pref = list(accumulate(nums, func=min))[:-2]
+    suf = list(accumulate(nums[::-1], func=min))[-3::-1]
+    return min([p + n + s for p, n, s in zip(pref, nums[1:-1], suf) if (p < n and n > s)], default=-1)
